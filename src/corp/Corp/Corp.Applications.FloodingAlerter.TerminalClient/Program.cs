78e1863f5f75f5d68fa8573eba94c42c98ffb131
Mads Mikkel Rasmussen <mara@aspit.dk>
2021-01-26 14:24:50 +0100

    Added Console app for testing.

---
+++ src/corp/Corp/Corp.Applications.FloodingAlerter.TerminalClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Corp.Applications.FloodingAlerter.TerminalClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
