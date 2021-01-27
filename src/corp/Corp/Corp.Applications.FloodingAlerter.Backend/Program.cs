d7d8b93315f5ebda5613835359d846aa965ae13a
Mads Mikkel Rasmussen <mara@aspit.dk>
2021-01-27 21:00:25 +0100

    Added backend project.

---
+++ src/corp/Corp/Corp.Applications.FloodingAlerter.Backend/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Corp.Applications.FloodingAlerter.Backend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
