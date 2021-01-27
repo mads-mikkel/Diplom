ce722e61b31304c72a85e8831ed7839063f1789e
Mads Mikkel Rasmussen <mara@aspit.dk>
2021-01-27 09:34:36 +0100

    Added Resources project.

---
+++ src/corp/Corp/Corp.Resources/Infrastructure/Endpoints.cs
@@ -0,0 +1,15 @@
+﻿namespace Corp.Resources.Infrastructure
+{
+    public static class Endpoints
+    {
+        public static class Applications
+        {
+            public const int FloodingAlerter = 2001;
+        }
+
+        public static class Services
+        {
+            public const int DataAccessServicePort = 3001;
+        }
+    }
+}
