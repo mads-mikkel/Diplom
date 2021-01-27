21cace14eaefc5b1cb01c5fea0a0f67a71af8ea0
Mads Mikkel Rasmussen <mara@aspit.dk>
2021-01-27 13:17:57 +0100

    SignalR Hub works.

---
+++ src/corp/Corp/Corp.Services.SignalRHub/ClockHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace Corp.Services.SignalRHub
+{
+    public class ClockHub:Hub<IClock>
+    {
+        public async Task SendTimeToClients(DateTime dateTime)
+        {
+            await Clients.All.ShowTime(dateTime);
+        }
+    }
+}
---
+++ src/corp/Corp/Corp.Services.SignalRHub/IClock.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Corp.Services.SignalRHub
+{
+    public interface IClock
+    {
+        Task ShowTime(DateTime dateTime);
+    }
+}
